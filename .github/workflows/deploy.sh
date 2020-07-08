a00cba4b8468212c0cb0332b59d6bd01cd616abf
Zhifei Yang <xividyzf@gmail.com>
2020-07-08 03:03:56 +0200

    Update deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,7 +6,7 @@ echo Generate book
 rm mdbook
 echo Checkout gh-pages
 git checkout gh-pages || git checkout -b gh-pages
-git pull origin gh-pages --rebase
+#git pull origin gh-pages --rebase
 echo ls@gh-pages
 ls -al
 echo Copy book sources
@@ -15,5 +15,5 @@ git clean -fx book
 git add .
 COMMIT_MESSAGE="Update mdbook `date '+%Y-%m-%d %H:%M:%S'`"
 git commit -a -m "${COMMIT_MESSAGE}"
-git push -u origin gh-pages
+git push -f origin gh-pages
 git checkout master
