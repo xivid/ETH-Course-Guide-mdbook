b2c4a3b56c94bfd11c9cd24d025685d2a5aa87d3
Zhifei Yang <xividyzf@gmail.com>
2020-07-08 02:47:00 +0200

    Create deploy.sh

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+wget https://github.com/Xivid/mdBook/releases/download/selfv1/mdbook
+chmod +x mdbook
+echo Generate book
+cd ETH-Course-Guide
+git checkout master
+../mdbook build
+cd ETH-Course-Guide
+git checkout gh-pages || git checkout -b gh-pages
+git pull origin gh-pages --rebase
+echo ls@gh-pages
+ls -al
+echo Copy book sources
+cp -R book/* .
+git clean -fx book
+git add .
+COMMIT_MESSAGE="Update mdbook `date '+%Y-%m-%d %H:%M:%S'`"
+git commit -a -m "${COMMIT_MESSAGE}"
+git push -u origin gh-pages
+git checkout master
